c2433edd2f314a92938d6ac36effce76958574ca
Matt Manning <mtmanning93@hotmail.co.uk>
2023-06-04 14:48:13 +0000

    Manually added host name

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-rwiqi9i8)@yhmy#3&@4gg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME'), 'django-todo-cruddin.herokuapp.com']
 
 
 # Application definition
