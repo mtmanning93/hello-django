227aaa25009d301bd9ed50fd9394d23b72e8eadd
Matt Manning <mtmanning93@hotmail.co.uk>
2023-06-04 15:24:55 +0000

    Attempt 2 at fix

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -34,7 +34,7 @@ DEBUG = development
 if development:
     ALLOWED_HOSTS = ['localhost']
 else:
-    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME'), 'django-todo-cruddin.herokuapp.com']
 
 
 # Application definition
